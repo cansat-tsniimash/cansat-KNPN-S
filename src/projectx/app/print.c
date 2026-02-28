74eccb36190f04456ca672d2395c94e2bdba4451
GruAnton <uldasevanton50@gmail.com>
2026-02-28 19:59:36 +0300

    Потерял файлы по дороге

---
+++ src/projectx/app/print.c
@@ -0,0 +1,16 @@
+/*
+ * print.c
+ *
+ *  Created on: Feb 20, 2026
+ *      Author: Anton
+ */
+#include "stm32f4xx.h"
+extern UART_HandleTypeDef huart2;
+
+
+
+int _write(int file, char *ptr, int len)
+{
+  HAL_UART_Transmit(&huart2, (uint8_t *)ptr, len, 100);
+  return len;
+}
